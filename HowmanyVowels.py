ad67fbe75e4d2a23e68fdf7804c4ed7c9789b630
Azazyro <42184192+Azazyro@users.noreply.github.com>
2018-08-15 19:57:41 -0500

    Create HowmanyVowels.py

---
+++ HowmanyVowels.py
@@ -0,0 +1,7 @@
+"""
+Create a function that takes a string and 
+returns the number (count) of vowels contained 
+within it
+"""
+def count_vowels(txt):
+	return sum([1 for x in txt.lower() if x in 'aeiou'])
