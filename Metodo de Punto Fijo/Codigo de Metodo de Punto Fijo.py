b32fb9200e80d2161284652a089924b459acaa3c
Azazyro <42184192+Azazyro@users.noreply.github.com>
2018-11-23 14:08:53 -0600

    Create Codigo de Metodo de Punto Fijo.py

---
+++ Metodo de Punto Fijo/Codigo de Metodo de Punto Fijo.py
@@ -0,0 +1,16 @@
+def xnew(x):
+    return (2*x**2 +3)/5
+
+x0 = 0
+x1 = 0
+itera = 0
+
+for i in range(100):
+    x1 = xnew(x0)
+    if abs(x1 - x0) < 0.00000001:
+        break
+    x0 = x1
+    itera += 1
+    
+print("La raiz es %.5f"%x1)
+print("Usando %i iteración"%itera)
