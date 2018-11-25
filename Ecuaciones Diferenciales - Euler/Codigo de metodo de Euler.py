e838b4f95d78d1ae82be6ad4c74a7f978bf0f8b4
Azazyro <42184192+Azazyro@users.noreply.github.com>
2018-11-25 13:37:17 -0600

    Create Codigo de metodo de Euler.py

---
+++ Ecuaciones Diferenciales - Euler/Codigo de metodo de Euler.py
@@ -0,0 +1,32 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def f(x,y):
+    return 2*x*y
+
+def euler(y0,fx,h,f):
+    y=[]
+    y.append(y0)
+    for i in range(1,len(x)):
+        y.append ( y[i-1] + h*f(x[i-1],y[i-1]) )
+    return y
+
+
+n = 11
+a = 1
+b = 1.5
+h = abs(a-b)/(n-1)
+x = np.linspace(a,b,n)
+
+
+y = euler(1,x,h,f)
+
+plt.plot(x,y,'b')
+plt.grid()
+
+print('Valor de h: ', h)
+
+print ('Valor final de x: ',x[-1])
+print ('Valor final de y: ',y[-1])
